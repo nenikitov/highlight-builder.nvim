c4ecf248cab31b9ac7aad8b9a4712c808f487d3a
nenikitov <nenikitov@mail.ru>
2023-02-05 20:37:42 -0500

    Created a skeleton for exported functions and put all the previously created util files

--- lua/highlight_builder/init.lua
+++ lua/highlight_builder/init.lua
@@ -1,1 +1,68 @@
+local utils_color = require('highlight_builder.utils.utils_color')
+
+--- Container for functions
+local H = {}
+
+
+--#region Types
+
+---@class HighlightCTerm Font modifiers avaiable for non-truecolor mode.
+---@field bold boolean Bold.
+---@field underline boolean Underline.
+---@field undercurl boolean Curly underline.
+---@field underdouble boolean Double underline.
+---@field underdotted boolean Dotted underline.
+---@field underdashed boolean Dashed underline.
+---@field strikethrough boolean Strike through.
+---@field reverse boolean Reverse background and foreground colors.
+---@field italic boolean Italicize.
+
+---@alias CTermColor number | nil | 'NONE' | 'Black' | 'DarkRed' | 'DarkGreen' | 'DarkYellow' | 'DarkBlue' | 'DarkMagenta' | 'DarkCyan' | 'Gray' | 'DarkGray' | 'Red' | 'Green' | 'Yellow' | 'Blue' | 'Magenta' | 'Cyan' | 'White'
+
+---@class HighlightGUI Highlight properties available to terminals in truecolor mode.
+---@field fg string GUI text color ('#RRGGBB' or color name).
+---@field bg string GUI background color ('#RRGGBB' or color name).
+---@field sp string GUI special color ('#RRGGBB' or color name).
+---@field bold boolean Bold.
+---@field underline boolean Underline.
+---@field undercurl boolean Curly underline.
+---@field underdouble boolean Double underline.
+---@field underdotted boolean Dotted underline.
+---@field underdashed boolean Dashed underline.
+---@field strikethrough boolean Strike through.
+---@field reverse boolean Reverse background and foreground colors.
+---@field italic boolean Italicize.
+
+---@class HighlightFull All highlight properties available.
+---@field ctermfg CTermColor Terminal text color.
+---@field ctermbg CTermColor Terminal background color.
+---@field cterm HighlightCTerm Terminal font modifiers.
+---@field fg string GUI text color ('#RRGGBB' or color name).
+---@field bg string GUI background color ('#RRGGBB' or color name).
+---@field sp string GUI special color ('#RRGGBB' or color name).
+---@field bold boolean Bold.
+---@field underline boolean Underline.
+---@field undercurl boolean Curly underline.
+---@field underdouble boolean Double underline.
+---@field underdotted boolean Dotted underline.
+---@field underdashed boolean Dashed underline.
+---@field strikethrough boolean Strike through.
+---@field reverse boolean Reverse background and foreground colors.
+---@field italic boolean Italicize.
+
+--#endregion
+
+
+--- Generate and append CTerm highlight properties to GUI only highlight.
+---@param gui HighlightGUI Only GUI highlights.
+---@return HighlightFull highlight Both GUI and CTerm highlight.
+function H.gui_to_cterm(gui)
+    return {}
+end
+
+
+H.mix_colors = utils_color.mix_colors
+
+
+return H
 
